aecd174518ea91f2764a8d0977186ea7ef2915a1
lavish-tak <lavish1854@gmail.com>
2024-09-03 22:41:58 +0530

    API added

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ const API_key = "60847f2a302151945777391ceece5023";
 currentTab.classList.add("current-tab");
 
 
-function switchtab(clickedTab){
+function switchTab(clickedTab){
     if(clickedTab!=currentTab){
        currentTab.classList.remove("current-tab");
        currentTab=clickedTab;
@@ -27,12 +27,51 @@ function switchtab(clickedTab){
        else{
         searchForm.classList.remove("active");
         userInfoContainer.classList.remove("active");
+        getFromSessionStorage();
        }
     }
 }
 userTab.addEventListener('click',()=>{
-    switchtab(userTab);
+    switchTab(userTab);
 })
 searchTab.addEventListener('click',()=>{
-    switchtab(searchTab);
+    switchTab(searchTab);
 })
+
+function getFromSessionStorage(){
+    const loalCoordinates = sessionStorage.getItem("user-coordinates");
+    if(!localCoordinates){
+        grantAccessContainer.classList.add("active");
+    }else{
+        const coordinates = JSON.parse(localCoordinates);
+        fetchUserWeatherInfo(coordinates);
+    }
+}
+async function fetchUserWeatherInfo(coordinates){
+    const {lat,lon} = coordinates;
+    grantAccessContainer.classList.remove("active");
+    loadingScreen.classList.add("active");
+    try{
+      const response = await fetch(
+         `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_key}`
+      );
+      const data = await response.json();
+      loadingScreen.classList.remove("active");
+      userInfoContainer.classList.add("active");
+      renderWeatherInfo(data);
+    }
+    catch(err){
+        loadingScreen.classList.remove("active");
+    }
+}
+
+ function renderWeatherInfo(weatherInfo){
+    const cityName = document.querySelector("[cityName]");
+    const countryIcon = document.querySelector("[countryIcon]");
+    const desc = document.querySelector("[weatherDesc]");
+    const weatherIcon = document.querySelector("[weatherIcon]");
+    const temp = document.querySelector("[data-temp]");
+    const windSpeed = document.querySelector("[data-windspeed]");
+    const humidity = document.querySelector("[data-humidity]");
+    const cloudiness = document.querySelector("[data-Clouds]");
+ }
